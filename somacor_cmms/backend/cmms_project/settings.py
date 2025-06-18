115803da559411ac88ede408b7a99dbc6fe5b499
matias morales <131200887+matiasmoralesa@users.noreply.github.com>
2025-06-18 15:21:36 -0400

    a

--- somacor_cmms/backend/cmms_project/settings.py
+++ somacor_cmms/backend/cmms_project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'cmms_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cmms_somacor',
+        'NAME': 'cmms_db',
         'USER': 'root', # Tu usuario de MySQL
         'PASSWORD': '21534', # La contraseña que definiste para MySQL
         'HOST': 'localhost',
